26b9afcad5b57e686ef03b4393dc3fdf730c7056
Luiz Henrique <luiz.henrique31415@gmail.com>
2026-05-26 15:08:11 -0300

    Make buildProjectPage use testing data

--- src/modules/build-pages.js
+++ src/modules/build-pages.js
@@ -3,25 +3,22 @@ import { getProjects } from "./handle-projects.js"
 
 
 const buildProjectPage = () => {
-    deleteHeaderElements()
-    deleteMainElements()
-    getProjects()
+    const projects = getProjects();
+
+    deleteHeaderElements();
+    deleteMainElements();
 
     let fragment = document.createDocumentFragment();
 
     const heading = createHeading(1, "My projects");
-    const projectItem = createProjectListItem("Home Tasks", 5, 'gray', 'border');
-    const projectItem1 = createProjectListItem("School Tasks", 2);
-    const projectItem2 = createProjectListItem("Art", 3);
-    const projectItem3 = createProjectListItem("Personal Tasks", 4);
-
     fragment.appendChild(heading);
-    fragment.appendChild(projectItem);
-    fragment.appendChild(projectItem1);
-    fragment.appendChild(projectItem2);
-    fragment.appendChild(projectItem3);
 
-    document.getElementsByTagName('main')[0].append(fragment)
+    projects.forEach(project => {
+        const projectElement = createProjectListItem(project.name, 5, 'gray', 'border');
+        fragment.appendChild(projectElement);
+    });
+
+    document.getElementsByTagName('main')[0].append(fragment);
 }
 
 const buildTodoList = () => {
